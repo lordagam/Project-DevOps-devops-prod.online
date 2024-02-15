d6a9da4371a22b1b4a31c697a80608e09ae36eb9
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-02-15 13:15:12 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "Project-DevOps-devops-prod.online-eks"
 }
 
 resource "random_string" "suffix" {
