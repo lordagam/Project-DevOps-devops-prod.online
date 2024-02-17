00e0c4d0b520f52c680f0ea8f84dd36c1f586052
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-02-17 21:27:51 +0200

    Add files via upload

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -70,20 +70,20 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t2.xlarge"]
+      instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 5
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
     }
 
     two = {
       name = "node-group-2"
 
-      instance_types = ["t2.xlarge"]
+      instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 5
+      max_size     = 2
       desired_size = 1
     }
   }
